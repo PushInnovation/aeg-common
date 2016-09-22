8d7511233a3681f4dfa82c4f500d6246514bddfb
Justin Durham <justin.durham@icloud.com>
2016-09-22 16:37:44 -0400

    Add each limit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import Base from './base';
 import BaseId from './base-id';
 import Service from './service';
 import forever from './promises/forever';
+import eachLimit from './promises/eachLimit';
 
-const ControlFlow = {forever};
+const ControlFlow = {forever, eachLimit};
 
 export { Base, BaseId, Service, ControlFlow };
 
