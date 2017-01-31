004e71491babf3d236fa7c0cb8feb936daff464e
Justin Durham <justin.durham@icloud.com>
2017-01-30 20:25:58 -0500

    Update whilst

--- src/errors/retry-whilst-cancel-error.js
+++ src/errors/retry-whilst-cancel-error.js
@@ -1,6 +1,31 @@
 /**
- * Retry whilst error
+ * Cancel a retry whilst
  */
 export default class RetryWhilstCancelError extends Error {
+
+	_innerError: ?Error;
+
+	/**
+	 * Returns the inner error
+	 * @returns {?Error}
+	 */
+	get innerError (): Error {
+
+		return this._innerError;
+
+	}
+
+	/**
+	 * Constructor
+	 * @param {string} message
+	 * @param {Error} err
+	 */
+	constructor (message: string, err: ?Error) {
+
+		super(message);
+
+		this._innerError = err;
+
+	}
 
 }
