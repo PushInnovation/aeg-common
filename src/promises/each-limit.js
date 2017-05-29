ff875725536ffc4021b625c6f855a009fd9e6b6c
Justin Durham <justin.durham@icloud.com>
2017-05-29 13:07:58 -0400

    Update flow

--- src/promises/each-limit.js
+++ src/promises/each-limit.js
@@ -3,9 +3,6 @@
 import async from 'async';
 import Promise from 'bluebird';
 
-type CallbackType = (err: ?Error) => void;
-type DelegateType = (value: any, callback: CallbackType) => void;
-
 /**
  * Run an array of promises with a set concurrency limit
  * @param {Object[]} arr
@@ -14,7 +11,7 @@ type DelegateType = (value: any, callback: CallbackType) => void;
  */
 export default async function eachLimit<T> (arr: T[], limit: number, delegate: (value: T) => ?Promise<void>): Promise<void> {
 
-	const el: (arr: Array<any>, limit: number, DelegateType) => Promise<void> = Promise.promisify(async.eachLimit, {context: async});
+	const el = Promise.promisify(async.eachLimit, {context: async});
 	return el(arr, limit, (value, callback) => {
 
 		Promise.resolve(delegate(value))
