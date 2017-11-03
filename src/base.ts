4b0d94d6de5e2c66402c1ec7d073dd6b91168e9a
jadurham909 <justin@adexchangegrp.com>
2017-11-03 17:13:17 -0400

    Fix binds

--- src/base.ts
+++ src/base.ts
@@ -79,7 +79,7 @@ export default class Base extends EventEmitter {
 
 		if (this._logger) {
 
-			this._log(this._logger.debug.bind(this), caller, options);
+			this._log(this._logger.debug, caller, options);
 
 		}
 
@@ -89,7 +89,7 @@ export default class Base extends EventEmitter {
 
 		if (this._logger) {
 
-			this._log(this._logger.info.bind(this), caller, options);
+			this._log(this._logger.info, caller, options);
 
 		}
 
@@ -99,7 +99,7 @@ export default class Base extends EventEmitter {
 
 		if (this._logger) {
 
-			this._log(this._logger.warn.bind(this), caller, options);
+			this._log(this._logger.warn, caller, options);
 
 		}
 
@@ -109,7 +109,7 @@ export default class Base extends EventEmitter {
 
 		if (this._logger) {
 
-			this._log(this._logger.error.bind(this), caller, options);
+			this._log(this._logger.error, caller, options);
 
 		}
 
